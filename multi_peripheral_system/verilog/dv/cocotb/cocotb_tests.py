3541f9c8bc715eaa27571a000ce6f0654957e50f
Mohamed Hosni <m.hosni.ibra@gmail.com>
2025-12-01 11:44:25 -0800

    "autosyncing agent activities"

--- multi_peripheral_system/verilog/dv/cocotb/cocotb_tests.py
+++ multi_peripheral_system/verilog/dv/cocotb/cocotb_tests.py
@@ -3,6 +3,6 @@ from test_pwm.test_pwm import tmr32_dv
 from test_uart.test_uart import uart_dv
 from test_spi.test_spi import spi_dv
 from test_i2c.test_i2c import i2c_dv
-from test_sram.test_sram import sram_dv
+from test_sram.test_sram import sram_test
 from test_adc.test_adc import adc_dv
 from test_system.test_system import system_integration_test
